8a97ac9c48cdf1f85991915521335594384b6e92
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-06 18:57:36 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,7 @@ module ArtistsHelper
     if song.artist
       link_to song.artist_name, artist_path(@artist)
     else
-      link_to 'Add an Artist', edit_song_path(song)
+      link_to 'Add Artist', edit_song_path(song)
     end
   end
 end
